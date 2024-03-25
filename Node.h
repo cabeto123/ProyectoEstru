64409cafd185c5682c93463093fede5c0398e4fa
FJPadillaOliva <fjpo@unitec.edu>
2024-03-25 17:24:36 -0600

    Documentacion de Nodo

--- Node.h
+++ Node.h
@@ -1,19 +1,28 @@
 #pragma once
 #include "Object.h"
+
 class Node
 {
 private:
-	Node* anterior;
-	Node* siguiente;
-	Object* contenido;
+    Node* anterior; // Puntero al nodo anterior
+    Node* siguiente; // Puntero al siguiente nodo
+    Object* contenido; // Contenido almacenado en el nodo
 public:
-	Node();
-	void setanterior(Node*);
-	void setsiguiente(Node*);
-	Node* getsiguiente();
-	Node* getanterior();
-	void setcontenido(Object*);
-	Object* getcontenido();
+    // Constructor por defecto
+    Node();
 
-	Node(Object*);
+    // Métodos para establecer y obtener el nodo anterior
+    void setanterior(Node*);
+    Node* getanterior();
+
+    // Métodos para establecer y obtener el siguiente nodo
+    void setsiguiente(Node*);
+    Node* getsiguiente();
+
+    // Métodos para establecer y obtener el contenido del nodo
+    void setcontenido(Object*);
+    Object* getcontenido();
+
+    // Constructor que inicializa el nodo con un objeto dado
+    Node(Object*);
 };
